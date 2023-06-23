b445c044eb20b8a8afd36b8f309ded8f2e1bc4d8
Raphael Paulino <raphael.paulino.web@gmail.com>
2023-06-22 23:33:55 -0300

    feat: create product index and product details pages

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc congue suscipit auctor. Ut suscipit turpis vel convallis vulputate. Quisque sem enim, congue non luctus quis, euismod nec massa.</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
