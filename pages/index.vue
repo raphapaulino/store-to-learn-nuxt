db37fc0b02a6b352e3e1337ea625278bf0afaee9
Raphael Paulino <raphael.paulino.web@gmail.com>
2023-06-22 23:45:16 -0300

    feat: add NuxtLink to navigate between the pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
     <div>
+        <header>
+            <nav>
+                <NuxtLink to="/">Nuxt Dojo</NuxtLink>
+                <ul>
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                    <li><NuxtLink to="/about">About</NuxtLink></li>
+                    <li><NuxtLink to="/products">Products</NuxtLink></li>
+                </ul>
+                <a href="/about">normal link -- about page</a>
+            </nav>
+        </header>
+
         <h2>Home</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc congue suscipit auctor. Ut suscipit turpis vel convallis vulputate. Quisque sem enim, congue non luctus quis, euismod nec massa.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc congue suscipit auctor. Ut suscipit turpis vel convallis vulputate. Quisque sem enim, congue non luctus quis, euismod nec massa.</p>
