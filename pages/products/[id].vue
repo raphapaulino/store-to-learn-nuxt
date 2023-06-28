47cf1d7cacb288f55323ecf3465a443a53f13837
Raphael Paulino <raphael.paulino.web@gmail.com>
2023-06-27 23:52:22 -0300

    feat: add custom error pages

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -11,6 +11,14 @@
     // fetch the product
     const { data: product } = await useFetch(uri)
 
+    if (!product.value) {
+        throw createError({ 
+            statusCode: 404, 
+            statusMessage: 'Product not found',
+            fatal: true
+        })
+    }
+
     definePageMeta({
         layout: 'products'
     })
